89493bed8ad0453de9bf9d3a863e3615f7134306
github username <github email address>
2018-01-29 13:11:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+//declare customerName to be bob in global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'test';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new test';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'Andrea';
+  let favoriteCustomer = 'Misanku';
+}
